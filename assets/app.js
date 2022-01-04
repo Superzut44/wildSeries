b536d54771ae9a383c688b28bf175487a2e8ff74
Restyled.io <commits@restyled.io>
2022-01-04 09:33:28 +0000

    Restyled by prettier

--- assets/app.js
+++ assets/app.js
@@ -6,26 +6,27 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import "./styles/app.scss";
 // start the Stimulus application
-import './bootstrap';
+import "./bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-const $ = require('jquery');
+const $ = require("jquery");
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
-require('bootstrap');
+require("bootstrap");
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
-$(document).ready(function() { $('[data-toggle="popover"]').popover(); });
+$(document).ready(function () {
+  $('[data-toggle="popover"]').popover();
+});
 
-document.querySelector("#watchlist").addEventListener('click', addToWatchlist);
+document.querySelector("#watchlist").addEventListener("click", addToWatchlist);
 
 function addToWatchlist(event) {
-
   event.preventDefault();
 
   // Get the link object you click in the DOM
@@ -33,25 +34,21 @@ function addToWatchlist(event) {
   let link = watchlistLink.href;
   // Send an HTTP request with fetch to the URI defined in the href
   fetch(link)
-      // Extract the JSON from the response
-      .then(res => res.json())
-      // Then update the icon
-      .then(function(res) {
-        let watchlistIcon = watchlistLink.firstElementChild;
-        if (res.isInWatchlist) {
-          watchlistIcon.classList.remove(
-              'bi-heart'); // Remove the .bi-heart (empty heart) from classes in
-                           // <i> element
-          watchlistIcon.classList.add(
-              'bi-heart-fill'); // Add the .bi-heart-fill (full heart) from
-                                // classes in <i> element
-        } else {
-          watchlistIcon.classList.remove(
-              'bi-heart-fill'); // Remove the .bi-heart-fill (full heart) from
-                                // classes in <i> element
-          watchlistIcon.classList.add(
-              'bi-heart'); // Add the .bi-heart (empty heart) from classes in
-                           // <i> element
-        }
-      });
+    // Extract the JSON from the response
+    .then((res) => res.json())
+    // Then update the icon
+    .then(function (res) {
+      let watchlistIcon = watchlistLink.firstElementChild;
+      if (res.isInWatchlist) {
+        watchlistIcon.classList.remove("bi-heart"); // Remove the .bi-heart (empty heart) from classes in
+        // <i> element
+        watchlistIcon.classList.add("bi-heart-fill"); // Add the .bi-heart-fill (full heart) from
+        // classes in <i> element
+      } else {
+        watchlistIcon.classList.remove("bi-heart-fill"); // Remove the .bi-heart-fill (full heart) from
+        // classes in <i> element
+        watchlistIcon.classList.add("bi-heart"); // Add the .bi-heart (empty heart) from classes in
+        // <i> element
+      }
+    });
 }
