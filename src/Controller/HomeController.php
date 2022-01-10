0b96ae5bd0892f66dfbb7abab91ff35d1b4549e3
Xavier CHAVAUDREY <chavaudreyxav@hotmail.fr>
2022-01-10 11:00:02 +0100

    quest 27 to finish

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    public function navbarTop(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('base.html.twig', [
+            'categories' => $categoryRepository->findBy([], ['id' => 'DESC'])
+        ]);
+    }
+}
+    
